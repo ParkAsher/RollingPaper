0ffc5ce2e9e5d33c4b54e32c8606beaef4b3725c
ByunJungMin <cvy5375@naver.com>
2023-01-14 18:07:47 +0900

    feat: createPost api

--- routes/post_router.js
+++ routes/post_router.js
@@ -1,24 +1,9 @@
 const express = require('express');
 const router = express();
-const Post = require('../schemas/post.js');
 
-router.post('/', async (req, res) => {
-    try {
-        const {nickname, content} = req.body;
-        console.log(nickname, content)
-        if (nickname === undefined) {
-            res.status(412).send({"message": "닉네임을 작성해 주세요"});
-        } else if (content === undefined) {
-            res.status(412).send({"message": "내용을 작성해 주세요"});
-        }
+const PostController = require('../controllers/post_controller');
+const postController = new PostController();
 
-        await Post.create({nickname, content});
-
-        res.status(201).send({"message": "롤링페이퍼 작성 완료"});
-
-    } catch (error) {
-        res.status(400).send({"message": "롤링페이퍼 작성에 실패하였습니다."});
-    }
-})
+router.post('/', postController.createPost);
 
 module.exports = router;
