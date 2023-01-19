66c3e2fcfc975f0c244a94f34016354d0546a757
boktato <fimjsl47@gmail.com>
2023-01-19 17:05:03 +0900

    save

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -5,21 +5,21 @@ class PostController {
 
     createPost = async (req, res) => {
         try {
-        const {nickname, content} = req.body;
+            const { nickname, content } = req.body;
 
-        if (nickname === undefined) {
-            res.status(412).send({"message": "닉네임을 작성해 주세요"});
-        } else if (content === undefined) {
-            res.status(412).send({"message": "내용을 작성해 주세요"});
-        }
+            if (nickname === undefined) {
+                res.status(412).send({ message: '닉네임을 작성해 주세요' });
+            } else if (content === undefined) {
+                res.status(412).send({ message: '내용을 작성해 주세요' });
+            }
 
-        await this.postService.createPost(nickname, content);
+            await this.postService.createPost(nickname, content);
 
-        res.status(201).send({"message": "롤링페이퍼 작성 완료"});
+            res.status(201).send({ message: '롤링페이퍼 작성 완료' });
         } catch (error) {
-            res.status(400).send({"message": "롤링페이퍼 작성에 실패하였습니다."});
+            res.status(400).send({ message: '롤링페이퍼 작성에 실패하였습니다.' });
         }
-    }
+    };
 }
 
 module.exports = PostController;
