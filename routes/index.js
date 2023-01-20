cf511e34978191d55ca9778900b819a880d0dfb4
ParkAsher <devasherpark@naver.com>
2023-01-20 23:47:24 +0900

    fix: User Router 선언

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // routes
-const postRouter = require("./post_router.js");
+const postRouter = require('./post_router.js');
+const userRouter = require('./user_router.js');
 
-router.use("/posts", postRouter);
+router.use('/posts', postRouter);
+router.use('/users', userRouter);
 
 module.exports = router;
