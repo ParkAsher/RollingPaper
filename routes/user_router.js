430e498448e620cd93897acebb58fcd9489c3e5b
Hojin Kim <miu.null@gmail.com>
2023-01-20 16:14:00 +0900

    test

--- routes/user_router.js
+++ routes/user_router.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express();
 
-const UserController = require('../controllers/user_controller');
+const UserController = require("../controllers/user_controller");
 const userController = new UserController();
 
-router.post('/login', userController.login);
+router.post("/login", userController.login);
 
 module.exports = router;
+// test
